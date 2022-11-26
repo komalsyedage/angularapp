c5156abc3446b73d203cb84f77978e3cfca44605
komalsyedage <komalsyedage@gmail.com>
2022-11-26 07:43:06 +0530

    Assignment
    Structure Directives-ngIf,ngFor,ngSwitch

--- src/app/ngif-for-switch/ngif-for-switch.component.ts
+++ src/app/ngif-for-switch/ngif-for-switch.component.ts
@@ -6,9 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ngif-for-switch.component.css']
 })
 export class NgifForSwitchComponent implements OnInit {
-  show:boolean=true;
+  select:string;
   constructor() { }
-
+  
+  getProductValue(value){
+    console.log(value.target.value);
+    this.select=value.target.value; //do this for when we click on mobile then only one mobile shown
+    
+  }
+ 
+condition:boolean;
+friendList=[ {name:'Raju',
+               age:25},
+              {name:'Sandhya',
+                age:23},
+              {name:'Mahesh',
+                age:24},
+              {name:'Ganesh',
+                age:24},
+              {name:'Radha',
+                 age:22},
+                {name:'Rahul',
+                  age:26}]
+gettrue(){
+  this.condition=true;
+}
+getfalse(){
+  this.condition=false;
+}
   ngOnInit() {
   }
 
