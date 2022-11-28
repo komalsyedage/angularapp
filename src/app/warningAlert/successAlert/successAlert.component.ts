269115f25451331c3426438425a21345a7594f76
komalsyedage <komalsyedage@gmail.com>
2022-11-28 21:50:42 +0530

    Remove  h1 to h2
    Remove  h1 to h2

--- src/app/warningAlert/successAlert/successAlert.component.ts
+++ src/app/warningAlert/successAlert/successAlert.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
 selector:'app-success',
-template:`<div class="this"><h1>You Are So Successful!</h1></div>`,
+template:`<div class="this"><h2>You Are So Successful!</h2></div>`,
 styleUrls:['./successAlert.component.css']
 
 })
--- src/app/warningAlert/warningAlert.component.ts
+++ src/app/warningAlert/warningAlert.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-warning',
-    template:`<div class=" this"><h1 >This is Warning , you are in danger!<h1><div>`,
+    template:`<div class=" this"><h2>This is Warning , you are in danger!<h2><div>`,
     styleUrls:['./warningAlert.component.css']
 })
 export class warningComponent{
